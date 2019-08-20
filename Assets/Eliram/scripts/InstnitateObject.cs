03a1d92be71f1183db503b660322abe3f9f6c036
Eliram <eliram13@gmail.com>
2019-08-20 13:47:27 +0900

    first playable build

--- Assets/Eliram/scripts/InstnitateObject.cs
+++ Assets/Eliram/scripts/InstnitateObject.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class InstnitateObject : MonoBehaviour
 {
-    public GameObject prefab;
+    public GameObject[] prefabs;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(prefab, new Vector3(0.0f, 1.0f, 5.0f), Quaternion.identity);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time % 3 == 0)
+        Instantiate(prefabs[Random.Range(0, 7) % 8], new Vector3(1000.0f, 0.0f, 0.0f), Quaternion.identity);
+       // plant.GetComponent<PlantGrowth>.lifeTime = 20;
+       // plant.GetComponent<PlantGrowth>.growthSpeed = 2;
     }
 }
