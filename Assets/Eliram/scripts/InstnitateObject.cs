54dc05b348f542d0f64a54c9705a6ceb53135f45
Eliram <eliram13@gmail.com>
2019-08-20 12:15:23 +0900

    add simple initilazie script

---
+++ Assets/Eliram/scripts/InstnitateObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstnitateObject : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab, new Vector3(0.0f, 1.0f, 5.0f), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
